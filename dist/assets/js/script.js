229c734018de586a21aea21ea6b4a92d5b82055c
ますだい <masunaga0424@gmail.com>
2023-11-19 22:19:54 +0900

    pc-navから！

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,4 +1,16 @@
 "use strict";
 
-jQuery(function ($) {// この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($('.js-hamburger').hasClass('is-active')) {
+      $('.js-hamburger').removeClass("is-active");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 });
