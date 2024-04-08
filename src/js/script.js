27c6c872b552b13f640802fc48796c0a04cf3868
ますだい <masunaga0424@gmail.com>
2024-04-08 22:40:16 +0900

    swiper動いた！page-topBTNも完了！

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,28 @@ jQuery(function ($) {
   });
 
   // campaignスライダー
+  const campaignSwiper = new Swiper(".js-campaign-swiper", {
+    loop: true, // ループ
+    speed: 1500, // 少しゆっくり(デフォルトは300)
+    slidesPerView: 1.25, // 一度に表示する枚数
+    breakpoints: {
+      1024: {
+        slidesPerView: 2.5, // 一度に表示する枚数
+        spaceBetween: 40, // スライド間の距離
+      },
+    },
+    spaceBetween: 24, // スライド間の距離
+    autoplay: {
+      // 自動再生
+      delay: 1000, // 1秒後に次のスライド
+      disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+    },
+    // 前後の矢印
+    navigation: {
+      nextEl: ".js-campaign-button-next",
+      prevEl: ".js-campaign-button-prev",
+    },
+  });
 
   // page-topボタン
   $(function () {
@@ -48,9 +70,9 @@ jQuery(function ($) {
     // フッター手前でストップ
     $(".js-page-top").hide();
     $(window).on("scroll", function () {
-      scrollHeight = $(document).height();
-      scrollPosition = $(window).height() + $(window).scrollTop();
-      footHeight = $("footer").innerHeight();
+      let scrollHeight = $(document).height();
+      let scrollPosition = $(window).height() + $(window).scrollTop();
+      let footHeight = $("footer").innerHeight();
       if (scrollHeight - scrollPosition <= footHeight) {
         // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
         $(".js-page-top").css({
